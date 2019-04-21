15b999a73b18ad47356a79ff4a72d7f20bf9de64
ranamoghazy <raromoghazy@gmail.com>
2019-04-21 03:52:21 +0200

    This is Rename bash script

---
+++ rename.sh
@@ -0,0 +1,21 @@
+TE=`date +%Y-%m-%d`
+echo "Please enter extension"
+
+
+read  ext
+var1=mycat.$ext
+
+
+echo "Please enter prefix"
+read  key
+
+   if [[ $key = "" ]]; then
+        var2="$DATE $var1"
+
+   else
+        var2="$key $var1"
+
+   fi
+
+ echo "Renaming from $var1 to $var2"
+
